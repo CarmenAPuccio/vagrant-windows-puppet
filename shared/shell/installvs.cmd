e7e7cafdde2a88d89edf8dbc5ef1fff1f675bc64
Carmen A. Pucciogit config --global user.email carmenpuccio@yahoo.com <carmenpuccio@yahoo.com>
2014-04-24 13:35:24 -0400

    Removing the PowerShell msdeploy reference

--- shared/shell/installvs.cmd
+++
@@ -1,6 +0,0 @@
-@echo 'Installing software...'
-
-@echo 'Install Visual Studio 2013'
-@powershell -NoProfile -ExecutionPolicy Bypass "cinst VisualStudio2013Ultimate -Version 12.0.21005.20140322"
-
-
