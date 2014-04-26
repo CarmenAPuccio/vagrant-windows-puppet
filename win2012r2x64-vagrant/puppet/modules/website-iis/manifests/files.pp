22bde09180e81c802a190376bef74405c397654d
Dexter Legaspi <dalegaspi@gmail.com>
2014-04-26 19:08:03 -0400

    changes to copy file from rndlabshare

--- win2012r2x64-vagrant/puppet/modules/website-iis/manifests/files.pp
+++ win2012r2x64-vagrant/puppet/modules/website-iis/manifests/files.pp
@@ -1,11 +1,26 @@
 
 class website-iis::files {
+
+	$username = 'RND\bizadmin'
+	 $password = 'bts@p2005'
+	 $share =  '\\rndlabshare1.rnd.local\arc\Installers\TeamCity\Redit'
+	 $safeshare = regsubst($share, '[\\$]', '.', 'G')
+
+	exec { 'install_mount':
+	   command => "$netuse $share /USER:$username $password"
+	 }
+
+
 	file { 'C:/inetpub/wwwroot/APRedit':
-		recurse => false,
+		recurse => true,
 		ensure => directory,
 		owner => 'Administrator',
 		group => 'Users',
 		mode => '7777',
 		notify => Service["W3SVC"],
+		source => "$share",
+		require => Exec['install_mount']
 	}
+
+
 }
