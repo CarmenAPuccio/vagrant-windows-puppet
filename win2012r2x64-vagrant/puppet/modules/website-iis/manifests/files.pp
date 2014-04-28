89d8989275981fc3a209e677fb1e105c48cca045
Dexter Legaspi <dalegaspi@gmail.com>
2014-04-28 11:24:18 -0400

    Revert "changes to copy file from rndlabshare"
    This reverts commit 22bde09180e81c802a190376bef74405c397654d.

--- win2012r2x64-vagrant/puppet/modules/website-iis/manifests/files.pp
+++ win2012r2x64-vagrant/puppet/modules/website-iis/manifests/files.pp
@@ -1,26 +1,11 @@
 
 class website-iis::files {
-
-	$username = 'RND\bizadmin'
-	 $password = 'bts@p2005'
-	 $share =  '\\rndlabshare1.rnd.local\arc\Installers\TeamCity\Redit'
-	 $safeshare = regsubst($share, '[\\$]', '.', 'G')
-
-	exec { 'install_mount':
-	   command => "$netuse $share /USER:$username $password"
-	 }
-
-
 	file { 'C:/inetpub/wwwroot/APRedit':
-		recurse => true,
+		recurse => false,
 		ensure => directory,
 		owner => 'Administrator',
 		group => 'Users',
 		mode => '7777',
 		notify => Service["W3SVC"],
-		source => "$share",
-		require => Exec['install_mount']
 	}
-
-
 }
