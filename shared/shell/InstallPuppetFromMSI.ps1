209c62212e7af4239ce89c4dce9213e25e719a71
Carmen A. Puccio <carmenpuccio@yahoo.com>
2015-02-19 15:08:21 -0500

    DesiredPuppetVersion Check
    Desired Puppet Version Check. Easier than constantly updating the base
    box.

--- shared/shell/InstallPuppetFromMSI.ps1
+++ shared/shell/InstallPuppetFromMSI.ps1
@@ -1,37 +1,64 @@
-$MsiUrl = "https://downloads.puppetlabs.com/windows/puppet-3.4.3.msi"
+$DesiredPuppetVersion = '3.7.4'
+$MsiUrl = "https://downloads.puppetlabs.com/windows/puppet-3.7.4.msi"
+$PuppetInstallerPath = 'c:\vagrantshared\resources\installers'
+$PuppetInstallerFile = 'puppet.msi'
+if ([System.IntPtr]::Size -eq 8) {
+  Write-Host "Going to Puppet 64-bit."
+  $MsiUrl = "https://downloads.puppetlabs.com/windows/puppet-3.7.4-x64.msi"
+  $PuppetInstallerFile = 'puppet-x64.msi'
+}
+$PuppetInstaller = Join-Path $PuppetInstallerPath $PuppetInstallerFile
 
 $PuppetInstalled = $false
+$PuppetVersionMatch = $false
 try {
   $ErrorActionPreference = "Stop";
   Get-Command puppet | Out-Null
   $PuppetInstalled = $true
   $PuppetVersion=&puppet "--version"
-  Write-Host "Puppet $PuppetVersion is installed. This process does not ensure the exact version or at least version specified, but only that puppet is installed. Exiting..."
-  Exit 0
+  if ($PuppetVersion -eq $DesiredPuppetVersion)
+  { 
+    Write-Host "Puppet $PuppetVersion is installed and matches. Exiting."
+    $PuppetVersionMatch = $true
+    $PuppetInstalled = $true
+    Exit 0
+  }
+
+  else
+  {
+  Write-Host "Puppet $PuppetVersion is installed but does not match. This is not the desired version which is $DesiredPuppetVersion. Continuing..."
+  }
+
 } catch {
-  Write-Host "Puppet is not installed, continuing..."
+  Write-Host "Puppet is not installed and/or isn't the desired version, continuing..."
 }
 
-if (!($PuppetInstalled)) {
+if (!($PuppetInstalled) -or !($PuppetVersionMatch)) {
   $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
   if (! ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
     Write-Host -ForegroundColor Red "You must run this script as an administrator."
     Exit 1
   }
 
+  if (!(Test-Path $PuppetInstallerPath)) {
+    Write-Host "Creating folder `'$PuppetInstallerPath`'"
+    $null = New-Item -Path "$PuppetInstallerPath" -ItemType Directory
+  }
+
+  if (!(Test-Path $PuppetInstaller)) {
+    Write-Host "Downloading `'$MsiUrl`' to `'$PuppetInstaller`'"
+    (New-Object Net.WebClient).DownloadFile("$MsiUrl","$PuppetInstaller")
+  }
+
+
   # Install it - msiexec will download from the url
-  $install_args = @("/qn", "/norestart","/i", $MsiUrl)
+  $install_args = @("/qn", "/norestart","/i", "$PuppetInstaller")
   Write-Host "Installing Puppet. Running msiexec.exe $install_args"
   $process = Start-Process -FilePath msiexec.exe -ArgumentList $install_args -Wait -PassThru
   if ($process.ExitCode -ne 0) {
     Write-Host "Installer failed."
     Exit 1
   }
-
-  # Stop the service that it autostarts
-  Write-Host "Stopping Puppet service that is running by default..."
-  Start-Sleep -s 5
-  Stop-Service -Name puppet
 
   Write-Host "Puppet successfully installed."
 }
